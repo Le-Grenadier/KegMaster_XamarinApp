ed578e8ab207499acbbee59b8ff6d2b252f9e477
John Grenard <John@JohnGrenard.com>
2019-10-16 20:09:02 -0700

    Remove unused "Call API" button

--- KegMaster/KegMaster.Core/Pages/MainPage.xaml.cs
+++ KegMaster/KegMaster.Core/Pages/MainPage.xaml.cs
@@ -60,39 +60,6 @@ namespace KegMaster.Core
                     await DisplayAlert($"Exception:", ex.ToString(), "Dismiss");
             }
         }
-        async void OnCallApi(object sender, EventArgs e)
-        {
-            try
-            {
-                lblApi.Text = $"Calling API {App.ApiEndpoint}";
-                var userContext = await authenticationService.SignInAsync();
-                var token = userContext.AccessToken;
-
-                // Get data from API
-                HttpClient client = new HttpClient();
-                HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, App.ApiEndpoint);
-                message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                HttpResponseMessage response = await client.SendAsync(message);
-                string responseString = await response.Content.ReadAsStringAsync();
-                if (response.IsSuccessStatusCode)
-                {
-                    lblApi.Text = $"Response from API {App.ApiEndpoint} | {responseString}";
-                }
-                else
-                {
-                    lblApi.Text = $"Error calling API {App.ApiEndpoint} | {responseString}";
-                }
-            }
-            catch (MsalUiRequiredException ex)
-            {
-                await DisplayAlert($"Session has expired, please sign out and back in.", ex.ToString(), "Dismiss");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert($"Exception:", ex.ToString(), "Dismiss");
-            }
-        }
-
         async void OnEditProfile(object sender, EventArgs e)
         {
             try
